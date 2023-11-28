30bd824c5d00a081ae9d95c56e56193b565fa4ac
mrizkiw <mrizkiwahyudin9h@gmail.com>
2023-11-29 00:50:18 +0700

    nambah database

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 const cors = require('cors');
 const fs = require("fs");
 
 
 const app = express();
 const PORT = 3000;
+
+// Setup MySQL connection
+const db = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+  });
+
+  db.connect(err => {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database');
+  });
 
 
 // pakai cors biar bisa share resource antar backend dan frontend
@@ -34,6 +50,21 @@ app.get("/articles", (req, res) => {
 		res.status(200).send(products);
 	});
 });
+
+app.get('/data/articles', (req, res) => {
+    // Ambil data dari tabel
+    const query = 'SELECT * FROM articles';
+    db.query(query, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        // Kirim data ke halaman baru
+        res.status(200).send(result);
+      }
+    });
+  });
+  
 
 // tangkap semua request/permintaan ke rute yang tidak dikenal
 app.all("*", (req, res) => {
